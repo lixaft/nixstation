e62338758c4f9e658c7cb6ecbe11eb126942df34
Fabien Taxil <me@lixaft.dev>
2024-07-10 19:01:18 +0200

    t

--- home/configs/nvim/after/queries/python/injections.scm
+++ home/configs/nvim/after/queries/python/injections.scm
@@ -8,3 +8,13 @@
  (#lua-match? @injection.content "^#: ")
  (#offset! @injection.content 0 3 0 0)
  (#set! injection.language "rst"))
+
+(call
+  function: (attribute
+    attribute: (identifier) @_attr)
+  arguments: (argument_list
+    .
+    (string
+      (string_content) @injection.content))
+  (#eq? @_attr "setStyleSheet")
+  (#set! injection.language "css"))
